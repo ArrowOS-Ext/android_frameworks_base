d49fbe234c98f2ff18d3e408a78232ba5528c2ad
yangbingqian <bqyang720@gmail.com>
2017-03-22 18:41:15 +0800

    FATAL EXCEPTION IN SYSTEM PROCESS: android.ui
    when 3rd party app put an extra with a bad serial object
    to start a service or sendbroadcast to system_server.
    It will cause system_server crash because of
    classNotFoundException.
    
    A test demo apk will cause system_server crash.
    It really affact the system stability
    
    Intent intent = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
    intent.putExtra(Intent.EXTRA_REFERRER, new serial()
    /*a Serializable class object*/);
    sendBroadcast(intent);
    
    Test: use the test apk attached in the issue below, system_server will
    crash every time. And it'll be OK with this CL.
    
    https://code.google.com/p/android/issues/detail?id=261031
    
    Change-Id: Id505571dcbf88c0ba9c1677ec0f1f08688c1bb95
    Signed-off-by: yangbingqian <bqyang720@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- core/java/android/os/BaseBundle.java
+++ core/java/android/os/BaseBundle.java
@@ -453,9 +453,15 @@ public class BaseBundle {
             } else {
                 throw e;
             }
+        } catch (RuntimeException e) {
+            if (sShouldDefuse && (e.getCause() instanceof ClassNotFoundException)) {
+                Log.w(TAG, "Failed to parse Bundle, but defusing quietly", e);
+                map.erase();
+            } else {
+                throw e;
+            }
         } finally {
             recycleParcel &= (numLazyValues[0] == 0);
-
             mMap = map;
             if (recycleParcel) {
                 recycleParcel(parcelledData);
