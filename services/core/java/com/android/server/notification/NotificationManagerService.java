c2e365e67983bd64a2bba48384873949171e903c
Edward Savage-Jones <edward.savage-jones@sony.com>
2022-10-31 16:28:19 +0100

    Notifications channels are not locked at first boot
    After first boot when doing S->T upgrade or when doing factory data
    reset on Android T, important system app notification channels are
    not greyed out/disabled as expected. DefaultPermissionGrantPolicy
    set fixed POST_NOTIFICATIONS are ignored.
    
    If a user then modifies these channels and reboots their device, they
    can lock these fixed notification channels into an incorrect state,
    where the only recovery is factory reset.
    
    Bug: 254576888
    Test: See issue
    Change-Id: If73e294b9a732ef47ad685fdd19f51afd8a04eef
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2333,7 +2333,6 @@ public class NotificationManagerService extends SystemService {
                 mUgmInternal,
                 new SysUiStatsEvent.BuilderFactory(),
                 mShowReviewPermissionsNotification);
-        mPreferencesHelper.updateFixedImportance(mUm.getUsers());
         mRankingHelper = new RankingHelper(getContext(),
                 mRankingHandler,
                 mPreferencesHelper,
@@ -2777,6 +2776,9 @@ public class NotificationManagerService extends SystemService {
         } else if (phase == SystemService.PHASE_ACTIVITY_MANAGER_READY) {
             mSnoozeHelper.scheduleRepostsForPersistedNotifications(System.currentTimeMillis());
             mAppLockManagerService = LocalServices.getService(AppLockManagerServiceInternal.class);
+        } else if (phase == SystemService.PHASE_DEVICE_SPECIFIC_SERVICES_READY) {
+            mPreferencesHelper.updateFixedImportance(mUm.getUsers());
+            mPreferencesHelper.migrateNotificationPermissions(mUm.getUsers());
         }
     }
 
